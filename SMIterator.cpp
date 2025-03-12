a45aac82e04f973b50ba04e3aee07d1008c29712
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-03-12 14:58:12 +0200

    Valid, Next, getCurrent

--- SMIterator.cpp
+++ SMIterator.cpp
@@ -22,29 +22,32 @@ void SMIterator::first(){ //default
 void SMIterator::next(){ //default
     //TODO - Implementation
 
-    if (index == 99) {
-        int i = 5;
-        int j=i;
-    }
+    // if (index == 99) {
+    //     int i = 5;
+    //     int j=i;
+    // }
 
     if (index + 1 < map.sizeOf) {
         index = index + 1;
     }
     else {
-        throw std::out_of_range("Iterator is out of range"); //exception if out of range
+        //throw std::out_of_range("Iterator is out of range"); //exception if out of range
+        index = map.sizeOf; //invalid if?????
     }
 }
 
 bool SMIterator::valid() const{ //default
     //TODO - Implementation
 
-    if (index < map.sizeOf) {
+    // if (map.sizeOf == 0) {
+    //     return false;
+    // }
+
+    if ((index < map.sizeOf) && (index >= 0)) { //TODO
         return true;
 
     }
-    else {
-        return false;
-    }
+    return false;
 
     //return false;
 }
@@ -57,6 +60,7 @@ TElem SMIterator::getCurrent() const{ //default
     }
     else {
         throw std::out_of_range("Not valid");
+        //return NULL_TPAIR;
     }
 
     //return NULL_TPAIR;
